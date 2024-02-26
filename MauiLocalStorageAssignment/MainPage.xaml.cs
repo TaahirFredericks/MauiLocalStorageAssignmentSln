7d05332797e3102d8dd8e7a9b1687d10f04ad7e1
TaahirFredericks <152262808+TaahirFredericks@users.noreply.github.com>
2024-02-26 14:38:44 +0200

    update

--- MauiLocalStorageAssignment/MainPage.xaml.cs
+++ MauiLocalStorageAssignment/MainPage.xaml.cs
@@ -1,60 +1,86 @@
 ﻿using Newtonsoft.Json;
 using System.ComponentModel;
 using System.Text.Json.Serialization;
+using System.Text;
 
 namespace MauiLocalStorageAssignment
 {
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
         private string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LocalStorageText.txt");
-      
-        public Profile _profile {  get; set; }
-       
+
+        private Profile _details;
+        public Profile Details
+        {
+            get => _details;
+            set
+            {
+                _details = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainPage()
         {
             InitializeComponent();
-            SaveBtn.Clicked += SaveBtn_Clicked;
+            Details = loadData();
+            BindingContext = this;
         }
 
-        #region properties
-
-        public class Profile
-        {
-            public string Name;
-            public string Surname;
-            public string EmailAddress;
-            public string Password;
-        }
-        #endregion
 
         #region eventhandlers
         private void SaveBtn_Clicked (object sender, EventArgs e)
         {
-            string name = NameEntry.Text;
+            SaveData(Details);
+            /*string name = NameEntry.Text;
             string surname = SurnameEntry.Text;
             string emailaddress = EmailEntry.Text;
             string password = PasswordEntry.Text;
 
             Profile profile = new Profile();
             {
-                name = name;
-                surname = surname;
-                emailaddress = emailaddress;
-                password = password;
-            };
+                Name = name;
+                Surname = surname;
+                EmailAddress = emailaddress;
+                Password = password;
+            };*/
 
+            
+
+        }
+
+        public void SaveData(Profile profile)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LocalStorageText.txt");
             string json = JsonConvert.SerializeObject(profile);
             File.WriteAllText(filePath, json);
-            TextBox.Text = File.ReadAllText(filePath);
+            // TextBox.Text = File.ReadAllText(filePath);
 
         }
         #endregion
 
-        private void LoadBtn_Clicked(object sender, EventArgs e)
+        public void LoadBtn_Clicked (object sender, EventArgs e)
         {
-            
+            Details = loadData();
         }
-    }
 
+        public static Profile loadData()
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LocalStorageText.txt");
+
+            if (File.Exists(filePath))
+            {
+                string currentprofile = File.ReadAllText(filePath);
+                Profile newdetails = JsonConvert.DeserializeObject<Profile>(currentprofile);
+                return newdetails;
+            }
+            else
+            {
+                return new Profile();
+            }
+
+
+        }
+       
+    }
 }
---
+++ MauiLocalStorageAssignment/Profile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiLocalStorageAssignment
+{
+    public partial class Profile : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+        private string _surname;
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Surname)));
+            }
+        }
+
+        private string _emailaddress;
+        public string EmailAddress
+        {
+            get => _emailaddress;
+            set
+            {
+                _emailaddress = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Email)));
+            }
+        }
+
+        private string _bio;
+
+        public string Bio
+        {
+            get => _bio;
+            set
+            {
+                _bio = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bio)));
+            }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Password)));
+            }
+        }
+    }
+}
